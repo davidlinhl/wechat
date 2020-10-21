27fc8d156218a57583843cdbaf8e9d612491a3b9
davidlin <davidlindev@qq.com>
2020-10-21 18:51:55 +0800

    a

--- tracker/util.py
+++ tracker/util.py
@@ -191,6 +191,7 @@ def get_last_group_mbr():
 
 
 def diff_mbr(old_mbr, new_mbr):
+    # TODO: 如果这个群之前没有
     join = []
     leave = []
     old_mbr = old_mbr.items()
@@ -254,7 +255,7 @@ def cal_join_leave():
     return len(joins), len(leaves)
 
 
-print(cal_join_leave())
+# print(cal_join_leave())
 
 # def cal_join_leave():
 #     """计算今天所有群进群和退群的人数.
